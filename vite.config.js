321b15e6ed2869c02abcb7b5020fa5edae6b975e
laurods <itrafor@gmail.com>
2026-02-06 18:34:55 -0300

    0602105

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [tailwindcss()],
   preview: {
-    allowedHosts: true, // Permite qualquer host no modo preview
+    allowedHosts: [
+      'app-loja.rbpezf.easypanel.host' // Adicione o seu domínio aqui
+    ],
     host: '0.0.0.0',
     port: 3000
   }
