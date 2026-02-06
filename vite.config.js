0661180fdaa93dee761047a91ac8f9f83dc33264
laurods <itrafor@gmail.com>
2026-02-06 18:31:40 -0300

    0602104

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [tailwindcss()],
+  preview: {
+    allowedHosts: true, // Permite qualquer host no modo preview
+    host: '0.0.0.0',
+    port: 3000
+  }
 })
